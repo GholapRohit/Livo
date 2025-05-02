d3ea160631f8f5cbefb1f7b6274d816aaf432adb
GholapRohit <rohitgholap2005@gmail.com>
2025-05-03 00:05:11 +0530

    remove

--- utils/expressError.js
+++
@@ -1,9 +0,0 @@
-class ExpressEror extends Error {
-  constructor(message, statusCode) {
-    super();
-    this.statusCode = statusCode;
-    this.message = message;
-  }
-}
-
-module.exports = ExpressEror;
